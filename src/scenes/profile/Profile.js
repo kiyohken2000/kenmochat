b74d04825133383765e93261720fdf92f1646e9c
kiyohken2000 <www.broadwell.com@gmail.com>
2021-05-29 14:21:23 +0900

    master

--- src/scenes/profile/Profile.js
+++ src/scenes/profile/Profile.js
@@ -37,9 +37,9 @@ export default function Profile(props) {
             />
           </View>
           <Text style={scheme === 'dark' ? styles.darkfield : styles.field}>Name:</Text>
-          <Text style={scheme === 'dark' ? styles.darktitle :styles.title}>{userData.fullName}</Text>
-          <Text style={scheme === 'dark' ? styles.darkfield :styles.field}>Mail:</Text>
-          <Text style={scheme === 'dark' ? styles.darktitle :styles.title}>{userData.email}</Text>
+          <Text style={scheme === 'dark' ? styles.darktitle : styles.title}>{userData.fullName}</Text>
+          <Text style={scheme === 'dark' ? styles.darkfield : styles.field}>Mail:</Text>
+          <Text style={scheme === 'dark' ? styles.darktitle : styles.title}>{userData.email}</Text>
           <TouchableOpacity style={styles.sbutton} onPress={goScan}>
             <Text style={styles.buttonText}>Scan</Text>
           </TouchableOpacity>
