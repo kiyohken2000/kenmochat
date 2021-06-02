23779699825f8124a45f5ef15ad3325d0629ac57
kiyohken2000 <www.broadwell.com@gmail.com>
2021-06-02 11:48:47 +0900

    master

--- src/scenes/stream/Stream.js
+++ src/scenes/stream/Stream.js
@@ -76,7 +76,14 @@ export default function Stream( props ) {
                         <Text style={scheme === 'dark' ? styles.darktitle : styles.title} numberOfLines={1}>{talk.name}</Text>
                         <Text style={scheme === 'dark' ? styles.darklatestMessage : styles.latestMessage} numberOfLines={1}>{talk.latestMessage.text}</Text>
                         <View style={styles.datecontainer}>
-                          {talk.id === 'Dj04ExLoykNI2sbmOVeW'?<Text style={scheme === 'dark' ? styles.darkbot : styles.bot}>🤖bot room🤖</Text>:null}
+                          {talk.id === 'Dj04ExLoykNI2sbmOVeW' ?
+                            (<Text style={scheme === 'dark' ? styles.darkbot : styles.bot}>🤖bot room🤖</Text>) :
+                            talk.id === 'o65qDbjlphSbs281TDX3' ?
+                            (<Text style={scheme === 'dark' ? styles.darkbot : styles.bot}>👁‍🗨Russian👁‍🗨</Text>) :
+                            talk.id === 'T5XMlAahT3dWwHfxFnqH' ?
+                            (<Text style={scheme === 'dark' ? styles.darkbot : styles.bot}>🔄Korean🔄</Text>) :
+                            null
+                          }
                           <Text style={scheme === 'dark' ? styles.darklatestDate : styles.latestDate}>{displaytime(talk.latestMessage.createdAt)}</Text>
                         </View>
                       </View>
