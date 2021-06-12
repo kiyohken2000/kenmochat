314242c748665be4c8eed7e5d6c907be5741a47b
kiyohken2000 <www.broadwell.com@gmail.com>
2021-06-13 00:50:36 +0900

    master

--- src/scenes/profile/Profile.js
+++ src/scenes/profile/Profile.js
@@ -5,10 +5,14 @@ import { firebase } from '../../firebase/config'
 import { Restart } from 'fiction-expo-restart'
 import { Avatar } from 'react-native-elements'
 import { QRCode } from 'react-native-custom-qr-codes-expo'
+import Dialog from "react-native-dialog"
+import Spinner from 'react-native-loading-spinner-overlay'
 
 export default function Profile(props) {
   const userData = props.extraData
   const scheme = useColorScheme()
+  const [visible, setVisible] = useState(false)
+  const [spinner, setSpinner] = useState(false)
 
   const goDetail = () => {
     props.navigation.navigate('Detail', { userData: userData })
@@ -17,6 +21,30 @@ export default function Profile(props) {
   const signOut = () => {
     firebase.auth().signOut()
     Restart()
+  }
+
+  const showDialog = () => {
+    setVisible(true)
+  }
+
+  const handleCancel = () => {
+    setVisible(false)
+  }
+
+  const accountDelete = async () => {
+    setSpinner(true)
+    const collectionRef = firebase.firestore()
+    await collectionRef.collection('tokens').doc(userData.email).delete()
+    await collectionRef.collection('users').doc(userData.id).delete()
+    await collectionRef.collection('users2').doc(userData.email).delete()
+    const user = firebase.auth().currentUser
+    user.delete().then(function() {
+      setSpinner(false)
+      firebase.auth().signOut()
+    }).catch(function(error) {
+      setSpinner(false)
+      console.log(error)
+    });
   }
 
   const goScan = () => {
@@ -46,6 +74,9 @@ export default function Profile(props) {
           <TouchableOpacity style={styles.button} onPress={goDetail}>
             <Text style={styles.buttonText}>Edit</Text>
           </TouchableOpacity>
+          <TouchableOpacity style={styles.deletebutton} onPress={showDialog}>
+            <Text style={styles.buttonText}>Account delete</Text>
+          </TouchableOpacity>
           <View style={styles.footerView}>
             <Text onPress={signOut} style={styles.footerLink}>Sign out</Text>
           </View>
@@ -58,6 +89,19 @@ export default function Profile(props) {
           </View>
         </View>
       </ScrollView>
+      <Dialog.Container visible={visible}>
+        <Dialog.Title>Account delete</Dialog.Title>
+        <Dialog.Description>
+          Do you want to delete this account? You cannot undo this action.
+        </Dialog.Description>
+        <Dialog.Button label="Cancel" onPress={handleCancel} />
+        <Dialog.Button label="Delete" onPress={accountDelete}  />
+      </Dialog.Container>
+      <Spinner
+        visible={spinner}
+        textStyle={{ color: "#fff" }}
+        overlayColor="rgba(0,0,0,0.5)"
+      />
     </View>
   )
 }
